4cb5a7ee1ac7608cf27802c60ac422addb597f77
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-04-25 11:10:41 +0200

    style: format project with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { Assets as NavigationAssets } from '@react-navigation/elements';
-import { Asset } from 'expo-asset';
-import * as SplashScreen from 'expo-splash-screen';
-import * as React from 'react';
-import { Navigation } from './navigation';
+import { Assets as NavigationAssets } from "@react-navigation/elements";
+import { Asset } from "expo-asset";
+import * as SplashScreen from "expo-splash-screen";
+import * as React from "react";
+import { Navigation } from "./navigation";
 
 Asset.loadAsync([
   ...NavigationAssets,
-  require('./assets/newspaper.png'),
-  require('./assets/bell.png'),
+  require("./assets/newspaper.png"),
+  require("./assets/bell.png"),
 ]);
 
 SplashScreen.preventAutoHideAsync();
@@ -16,10 +16,10 @@ export function App() {
   return (
     <Navigation
       linking={{
-        enabled: 'auto',
+        enabled: "auto",
         prefixes: [
           // Change the scheme to match your app's scheme defined in app.json
-          'helloworld://',
+          "helloworld://",
         ],
       }}
       onReady={() => {
--- src/navigation/screens/Settings.tsx
+++ src/navigation/screens/Settings.tsx
@@ -1,5 +1,5 @@
-import { Text } from '@react-navigation/elements';
-import { StyleSheet, View } from 'react-native';
+import { Text } from "@react-navigation/elements";
+import { StyleSheet, View } from "react-native";
 
 export function Settings() {
   return (
@@ -12,12 +12,12 @@ export function Settings() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     gap: 10,
   },
   row: {
-    flexDirection: 'row',
+    flexDirection: "row",
     gap: 10,
   },
 });
