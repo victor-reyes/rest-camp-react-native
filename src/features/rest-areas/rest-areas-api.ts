61ba7603d12ae33e48a596eb771e14bed611922e
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-04-30 16:07:05 +0200

    feat: add getParkingById query to fetch parking details by ID

--- src/features/rest-areas/rest-areas-api.ts
+++ src/features/rest-areas/rest-areas-api.ts
@@ -1,14 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { Parking, Response, ResponseSchema } from "./parking-zod-schema";
 
-const PARKING_REQUEST_BODY = `<?xml version="1.0" encoding="utf-8"?>
-<REQUEST> 
-    <LOGIN authenticationkey="${process.env.EXPO_PUBLIC_TRAFIK_INFO_API_KEY}"/>
-    <QUERY 
-        objecttype="Parking" 
-        schemaversion="1.4"/>
-</REQUEST>`;
-
 export const restAreasApi = createApi({
   reducerPath: "restAreasApi",
   baseQuery: fetchBaseQuery({
@@ -23,13 +15,39 @@ export const restAreasApi = createApi({
       query: () => ({
         url: "",
         method: "POST",
-        body: PARKING_REQUEST_BODY,
+        body: getBody(),
       }),
       rawResponseSchema: ResponseSchema,
       transformResponse: async (response: Response) =>
         response?.RESPONSE?.RESULT?.[0]?.Parking || [],
     }),
+    getParkingById: builder.query<Parking | undefined, string>({
+      query: (id: string) => ({
+        url: "",
+        method: "POST",
+        body: getBody(`<EQ name=\"Id\" value=\"${id}\" />`),
+      }),
+      rawResponseSchema: ResponseSchema,
+      transformResponse: async (response: Response) => {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        return response?.RESPONSE?.RESULT?.[0]?.Parking?.at(0);
+      },
+    }),
   }),
 });
 
-export const { useGetParkingsQuery } = restAreasApi;
+function getBody(filter: string = "") {
+  return `<?xml version="1.0" encoding="utf-8"?>
+<REQUEST> 
+    <LOGIN authenticationkey="${process.env.EXPO_PUBLIC_TRAFIK_INFO_API_KEY}"/>
+    <QUERY 
+        objecttype="Parking" 
+        schemaversion="1.4">
+      <FILTER>
+        ${filter}
+      </FILTER>
+    </QUERY>
+</REQUEST>`;
+}
+
+export const { useGetParkingsQuery, useGetParkingByIdQuery } = restAreasApi;
