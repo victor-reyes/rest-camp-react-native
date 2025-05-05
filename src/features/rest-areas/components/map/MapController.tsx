ff5fa4bd7fe6c90e3249350994c2ce3067e9ca46
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-05-05 17:31:46 +0200

    refactor: simplify MapControls

--- src/features/rest-areas/components/map/MapController.tsx
+++ src/features/rest-areas/components/map/MapController.tsx
@@ -1,6 +1,7 @@
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import * as Location from "expo-location";
-import { Alert } from "react-native";
+import { Alert, Pressable, StyleSheet } from "react-native";
+
 interface Props {
   onLocationUpdate: (coords: { latitude: number; longitude: number }) => void;
 }
@@ -18,18 +19,25 @@ export function MapControls({ onLocationUpdate }: Props) {
   };
 
   return (
-    <MaterialIcons
-      name="gps-fixed"
-      size={24}
-      color="gray"
-      zIndex={1000}
-      position="absolute"
-      bottom={20}
-      right={20}
-      backgroundColor="#ffffffdd"
-      padding={10}
-      style={{ borderRadius: 5, zIndex: 1000 }}
+    <Pressable
       onPress={handleGetUserLocation}
-    />
+      style={({ pressed }) => [styles.button, pressed && styles.buttonPressed]}>
+      <MaterialIcons name="gps-fixed" size={24} color="gray" />
+    </Pressable>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+    backgroundColor: "#ffffffcc",
+    padding: 10,
+    borderRadius: 5,
+    zIndex: 100,
+  },
+  buttonPressed: {
+    backgroundColor: "#fff",
+  },
+});
