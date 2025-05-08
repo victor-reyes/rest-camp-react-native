6db8907003bafab6989860172e8cfad1c5fcd718
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-05-08 12:11:52 +0200

    ref(types): rename ParkingItem to Parking

--- src/features/rest-areas/types.ts
+++ src/features/rest-areas/types.ts
@@ -1,6 +1,8 @@
 import { parkings, photos, services } from "@/db";
+import { z } from "zod";
+import { ResponseSchema } from "./api";
 
-export type ParkingPoint = ParkingItem & { type: "Point" };
+export type ParkingPoint = Parking & { type: "Point" };
 export type ParkingCluster = {
   type: "Cluster";
   id: string;
@@ -13,10 +15,12 @@ export type ParkingCluster = {
 
 export type Filter = (typeof services.$inferSelect)["name"];
 
-export type ParkingItem = typeof parkings.$inferSelect & {
+export type Parking = typeof parkings.$inferSelect & {
   services: (typeof services.$inferSelect)[];
   photos: (typeof photos.$inferSelect)[];
 };
 
 export type Photo = typeof photos.$inferSelect;
 export type Service = typeof services.$inferSelect;
+
+export type ParkingResponse = z.infer<typeof ResponseSchema>;
