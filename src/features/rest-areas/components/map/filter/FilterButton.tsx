d739e423dbfa28cd3ee0ebad9eccba64f9b0e54f
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-05-10 17:39:29 +0200

    refactor(Filters): remove FilterButton component and use FontAwesome directly

--- src/features/rest-areas/components/map/filter/FilterButton.tsx
+++
@@ -1,5 +0,0 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-
-export function FilterButton() {
-  return <FontAwesome name="filter" size={24} color="#aaa" />;
-}
--- src/features/rest-areas/components/map/filter/Filters.tsx
+++ src/features/rest-areas/components/map/filter/Filters.tsx
@@ -1,10 +1,11 @@
 import { FilterButton } from "./FilterButton";
 import { FilterPopup } from "./FilterPopup";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export function Filters() {
   return (
     <FilterPopup>
-      <FilterButton />
+      <FontAwesome name="filter" size={24} color="#aaa" />
     </FilterPopup>
   );
 }
