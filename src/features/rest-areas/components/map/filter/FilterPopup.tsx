bac59d88337283851210811e0d7df37bbff26b36
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-05-11 13:34:57 +0200

    feat(FilterPopup): add modal to handle clicks outside filter popup

--- src/features/rest-areas/components/map/filter/FilterPopup.tsx
+++ src/features/rest-areas/components/map/filter/FilterPopup.tsx
@@ -1,4 +1,4 @@
-import { View, Pressable, Text, StyleSheet } from "react-native";
+import { View, Pressable, Text, StyleSheet, Modal, Dimensions } from "react-native";
 import { useAppDispatch, useAppSelector } from "@/app/store";
 import {
   filterAdded,
@@ -8,8 +8,9 @@ import {
 } from "@/features/rest-areas/rest-area-slice";
 import { Filter } from "@/features/rest-areas/types";
 import { FilterItem } from "./FilterItem";
-import { useState } from "react";
+import { ReactNode, useLayoutEffect, useRef, useState } from "react";
 import { FilterCountBadge } from "./FilterCountBadge";
+import equal from "fast-deep-equal";
 
 const filterOptions: Filter[] = [
   "refuseBin",
@@ -23,7 +24,7 @@ const filterOptions: Filter[] = [
 ];
 
 interface Props {
-  children: React.ReactNode;
+  children: ReactNode;
 }
 
 export function FilterPopup({ children: trigger }: Props) {
@@ -36,34 +37,52 @@ export function FilterPopup({ children: trigger }: Props) {
   const handleFilterPress = (filter: Filter, isEnabled: boolean) =>
     dispatch(isEnabled ? filterAdded(filter) : filterRemoved(filter));
 
+  const triggerContainerRef = useRef<View>(null);
+  const [popupPosition, setPopupPosition] = useState({ bottom: 0, right: 0 });
+
+  useLayoutEffect(() => {
+    if (open && triggerContainerRef.current) {
+      triggerContainerRef.current.measureInWindow((x, y, triggerW) => {
+        const { width, height } = Dimensions.get("window");
+        const newPosition = { bottom: height - y + 8, right: width - x - triggerW };
+
+        if (!equal(newPosition, popupPosition)) setPopupPosition(newPosition);
+      });
+    }
+  }, [open, popupPosition]);
+
   return (
     <>
       {open && (
-        <View style={styles.container}>
-          {filterOptions.map((filter, index, array) => (
-            <View key={filter}>
-              <FilterItem
-                key={filter}
-                filter={filter}
-                isActive={filters.includes(filter)}
-                onValueChange={isEnabled => handleFilterPress(filter, isEnabled)}
-              />
-              {index < array.length - 1 && <View style={styles.divider} />}
-            </View>
-          ))}
+        <Modal visible={open} transparent animationType="fade">
+          <Pressable onPress={() => setOpen(false)} style={styles.outerContainer} />
+          <View style={[styles.container, popupPosition]}>
+            {filterOptions.map((filter, index, array) => (
+              <View key={filter}>
+                <FilterItem
+                  key={filter}
+                  filter={filter}
+                  isActive={filters.includes(filter)}
+                  onValueChange={isEnabled => handleFilterPress(filter, isEnabled)}
+                />
+                {index < array.length - 1 && <View style={styles.divider} />}
+              </View>
+            ))}
 
-          <View style={styles.buttonsContainer}>
-            <Pressable style={styles.button} onPress={() => setOpen(false)}>
-              <Text style={styles.buttonText}>Använd</Text>
-            </Pressable>
-            <Pressable style={styles.button} onPress={() => dispatch(filtersCleared())}>
-              <Text style={styles.buttonText}>Rensa</Text>
-            </Pressable>
+            <View style={styles.buttonsContainer}>
+              <Pressable style={styles.button} onPress={() => setOpen(false)}>
+                <Text style={styles.buttonText}>Använd</Text>
+              </Pressable>
+              <Pressable style={styles.button} onPress={() => dispatch(filtersCleared())}>
+                <Text style={styles.buttonText}>Rensa</Text>
+              </Pressable>
+            </View>
           </View>
-        </View>
+        </Modal>
       )}
 
       <Pressable
+        ref={triggerContainerRef}
         onPress={handlePress}
         style={({ pressed }) => [styles.triggerContainer, pressed && { backgroundColor: "#eee" }]}>
         <FilterCountBadge count={filters.length} />
@@ -91,7 +110,15 @@ const styles = StyleSheet.create({
     elevation: 2,
     alignSelf: "flex-end",
   },
+  outerContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
   container: {
+    position: "absolute",
     backgroundColor: "#fff",
     borderRadius: 12,
     padding: 10,
