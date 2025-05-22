94c6c8c8d9573016561ac3293e6e943a0fb9c439
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-05-22 13:42:35 +0200

    feat: add Input component with customizable label and icons

---
+++ src/components/Input.tsx
@@ -0,0 +1,52 @@
+import { StyleSheet, Text, View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+
+interface Props extends React.ComponentProps<typeof TextInput> {
+  label?: string;
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
+}
+
+export function Input({ label, leftIcon, rightIcon, ...props }: Props) {
+  return (
+    <View style={styles.container}>
+      {label && <Text style={styles.label}>{label}</Text>}
+      <View style={styles.inputContainer}>
+        {leftIcon}
+        <TextInput
+          style={styles.input}
+          placeholderTextColor="#ccc"
+          autoCapitalize={"none"}
+          {...props}
+        />
+        {rightIcon}
+      </View>
+      <View style={styles.horizontalLine} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    maxWidth: 320,
+    marginBottom: 16,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 8,
+  },
+  label: {
+    fontSize: 16,
+  },
+  input: {
+    flex: 1,
+    padding: 8,
+  },
+  horizontalLine: {
+    height: 1,
+    backgroundColor: "#aaa",
+  },
+});
