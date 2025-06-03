1c8f411f0f3a98a7d741232c4be33b4d2b2246d3
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-06-03 12:50:41 +0200

    feat(supabase): specify type for Supabase client using Database type

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,11 +3,12 @@ import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 import { processLock } from "@supabase/auth-js/src/lib/locks";
+import { Database } from "./database.types";
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
