aadf488de2e76b362bf7adc0e0a6bde6f9801f2c
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-06-04 10:44:45 +0200

    feat(types): add types for RestAreaApiResponse, Photo, Service, and RestAreaWithServicesAndPhotos

---
+++ supabase/functions/update-rest-areas/types.ts
@@ -0,0 +1,13 @@
+import z from "zod";
+import { Database } from "./database.types.ts";
+import { ResponseSchema } from "./schemas/response-schema.ts";
+
+export type RestAreaApiResponse = z.infer<typeof ResponseSchema>;
+
+export type Photo = Omit<Database["public"]["Tables"]["photos"]["Insert"], "rest_area_id">;
+export type Service = Omit<Database["public"]["Tables"]["services"]["Insert"], "rest_area_id">;
+export type RestArea = Database["public"]["Tables"]["rest_areas"]["Insert"];
+export type RestAreaWithServicesAndPhotos = RestArea & {
+  services: Service[];
+  photos: Photo[];
+};
