ae26205482ba3af29c90b314e91b502f3a3e0145
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-06-06 17:22:28 +0200

    refactor(AddPhotoButton): replace button with popover for photo selection

--- src/screens/rest-area/components/AddPhotoButton.tsx
+++ src/screens/rest-area/components/AddPhotoButton.tsx
@@ -1,28 +1,66 @@
-import { Pressable, StyleSheet, Text } from "react-native";
-import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome6";
+import Popover from "react-native-popover-view";
 
 export function AddPhotoButton() {
   return (
-    <Pressable
-      onPress={() => console.log("Add photo")}
-      hitSlop={8}
-      style={({ pressed }) => [styles.addPhotoButton, pressed && { backgroundColor: "#e0e0e0" }]}>
-      <FontAwesome6 name="plus" size={24} color="#155196" />
-      <Text>Lägg till foto</Text>
-    </Pressable>
+    <Popover
+      from={
+        <Pressable
+          hitSlop={8}
+          style={({ pressed }) => [styles.button, pressed && { backgroundColor: "#e0e0e0" }]}>
+          <FontAwesome6 name="plus" size={24} color="#155196" />
+          <Text>Lägg till foto</Text>
+        </Pressable>
+      }
+      popoverStyle={styles.popoverContainer}
+      arrowSize={{ width: 0, height: 0 }}>
+      <View style={{ gap: 8 }}>
+        <View>
+          <Pressable
+            hitSlop={8}
+            style={({ pressed }) => [styles.button, pressed && { backgroundColor: "#e0e0e0" }]}>
+            <FontAwesome5 name="camera" size={24} color="#155196" />
+            <Text>Kamera</Text>
+          </Pressable>
+        </View>
+        <View>
+          <Pressable
+            hitSlop={8}
+            style={({ pressed }) => [styles.button, pressed && { backgroundColor: "#e0e0e0" }]}>
+            <FontAwesome5 name="images" size={24} color="#155196" />
+            <Text>Gallery</Text>
+          </Pressable>
+        </View>
+      </View>
+    </Popover>
   );
 }
 
 const styles = StyleSheet.create({
-  addPhotoButton: {
+  button: {
     flexDirection: "row",
-    gap: 4,
-    padding: 4,
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+    gap: 16,
     borderRadius: 4,
     borderColor: "#eee",
     borderWidth: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  popoverContainer: {
+    padding: 8,
+    borderRadius: 4,
+    backgroundColor: "#ffffff",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
   },
 });
