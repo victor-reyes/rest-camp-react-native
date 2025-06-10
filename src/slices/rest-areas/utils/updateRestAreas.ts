0629058496c4c396c365f8d81123aff4a9d71219
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-06-10 12:22:20 +0200

    feat(updateRestAreas): only update/insert `modified` or `new` data

--- src/slices/rest-areas/utils/updateRestAreas.ts
+++ src/slices/rest-areas/utils/updateRestAreas.ts
@@ -1,21 +1,30 @@
 import { db, restAreas, photos, services } from "@/db";
 import { PhotoInsert, RestAreaInsert, ServiceInsert } from "../types";
+import { inArray } from "drizzle-orm";
 
 export async function updateRestAreas(data: {
   restAreas: RestAreaInsert[];
   services: ServiceInsert[];
   photos: PhotoInsert[];
 }) {
-  await db.transaction(async tx => {
+  return await db.transaction(async tx => {
     try {
-      await tx.delete(restAreas);
-      await tx.delete(services);
-      await tx.delete(photos);
+      const [restAreaIds, servicesIds, photosIds] = [
+        data.restAreas.map(r => r.id),
+        data.services.map(s => s.restAreaId),
+        data.photos.map(p => p.restAreaId),
+      ];
+      await tx.delete(restAreas).where(inArray(restAreas.id, restAreaIds));
+      await tx.delete(services).where(inArray(services.restAreaId, servicesIds));
+      await tx.delete(photos).where(inArray(photos.restAreaId, photosIds));
+
       await tx.insert(restAreas).values(data.restAreas);
       await tx.insert(services).values(data.services);
       await tx.insert(photos).values(data.photos);
+      return { error: null };
     } catch (error) {
       console.error("Error inserting data into the database:", error);
+      return { error };
     }
   });
 }
