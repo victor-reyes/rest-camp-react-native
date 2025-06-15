a8953bbbf0061b2c2d39e3b640e7db94db5c4c78
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-06-15 19:32:27 +0200

    fix(types): ensure  all field of RestArea type are required to proper upsert

--- supabase/functions/update-rest-areas/types.ts
+++ supabase/functions/update-rest-areas/types.ts
@@ -6,7 +6,7 @@ export type RestAreaApiResponse = z.infer<typeof ResponseSchema>;
 
 export type Photo = Omit<Database["public"]["Tables"]["photos"]["Insert"], "rest_area_id">;
 export type Service = Omit<Database["public"]["Tables"]["services"]["Insert"], "rest_area_id">;
-export type RestArea = Database["public"]["Tables"]["rest_areas"]["Insert"];
+export type RestArea = Required<Omit<Database["public"]["Tables"]["rest_areas"]["Insert"], "id">>;
 export type RestAreaWithServicesAndPhotos = RestArea & {
   services: Service[];
   photos: Photo[];
