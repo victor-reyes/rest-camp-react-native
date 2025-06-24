6735157d52fb09249025fd7e8f3c1c8b78ff31aa
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-06-24 20:12:50 +0200

    refactor(types): reorganize and rename types for clarity and consistency

--- admin/src/api/supabase/types.ts
+++ admin/src/api/supabase/types.ts
@@ -1,11 +1,19 @@
 import type { Database } from "./database.types.ts";
 
-export type Photo = Database["public"]["Tables"]["photos"]["Insert"];
-export type Service = Database["public"]["Tables"]["services"]["Insert"];
-export type RestArea = Database["public"]["Tables"]["rest_areas"]["Insert"];
-export type Review = Database["public"]["Tables"]["reviews"]["Insert"];
-export type RestAreaWithInfo = RestArea & {
-  services: Service[];
-  photos: Photo[];
-  reviews: Review[];
+export type RestAreaInsert = Database["public"]["Tables"]["rest_areas"]["Insert"];
+export type PhotoInsert = Database["public"]["Tables"]["photos"]["Insert"];
+export type ServiceInsert = Database["public"]["Tables"]["services"]["Insert"];
+
+export type RestAreaWithInfoInsert = RestAreaInsert & {
+  services: ServiceInsert[];
+  photos: PhotoInsert[];
+};
+
+export type RestAreaSelect = Database["public"]["Tables"]["rest_areas"]["Row"];
+export type ServiceSelect = Database["public"]["Tables"]["services"]["Row"];
+export type PhotoSelect = Database["public"]["Tables"]["photos"]["Row"];
+
+export type RestAreaWithInfo = RestAreaSelect & {
+  services: ServiceSelect[];
+  photos: PhotoSelect[];
 };
