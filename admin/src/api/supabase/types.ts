ce6f2ae195d8f9c7b7075c72daac8cc0d8aa4af4
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-06-24 12:25:36 +0200

    feat(types): add Review type to RestAreaWithServicesAndPhotos

--- admin/src/api/supabase/types.ts
+++ admin/src/api/supabase/types.ts
@@ -3,7 +3,9 @@ import type { Database } from "./database.types.ts";
 export type Photo = Database["public"]["Tables"]["photos"]["Insert"];
 export type Service = Database["public"]["Tables"]["services"]["Insert"];
 export type RestArea = Database["public"]["Tables"]["rest_areas"]["Insert"];
+export type Review = Database["public"]["Tables"]["reviews"]["Insert"];
 export type RestAreaWithServicesAndPhotos = RestArea & {
   services: Service[];
   photos: Photo[];
+  reviews: Review[];
 };
