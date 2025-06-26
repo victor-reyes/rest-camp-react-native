994ed212b1364a7bc38ad93bad4f0faf9dfd702a
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-06-26 13:52:56 +0200

    feat(utils): add sortByUpdatedAt function for sorting RestAreas by updated date

--- admin/src/features/rest-areas/utils/utils.ts
+++ admin/src/features/rest-areas/utils/utils.ts
@@ -5,3 +5,8 @@ export const equalByTrafikverketId = (a: RestAreaWithInfo, b: RestAreaWithInfo) 
 
 export const equalByCoordinates = (a: RestAreaWithInfo, b: RestAreaWithInfo) =>
   a.latitude === b.latitude && a.longitude === b.longitude;
+
+export const sortByUpdatedAt = (a: RestAreaWithInfo, b: RestAreaWithInfo) => {
+  if (a.updated_at === b.updated_at) return 0;
+  return a.updated_at > b.updated_at ? -1 : 1;
+};
