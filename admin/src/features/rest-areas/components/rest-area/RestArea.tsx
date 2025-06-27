8cf56d96256d9f5682208a5bc4a12fb9299cf620
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-06-27 18:57:47 +0200

    feat(admin/RestArea): implement state management for rest area

--- admin/src/features/rest-areas/components/rest-area/RestArea.tsx
+++ admin/src/features/rest-areas/components/rest-area/RestArea.tsx
@@ -5,6 +5,8 @@ import { RestAreaStatus } from "./RestAreaStatus";
 import { RestAreaDescription } from "./RestAreaDescription";
 import { RestAreaGallery } from "./RestAreaGallery";
 import { RestAreaServices } from "./RestAreaServices";
+import type { Service, Status } from "../../types";
+import { useEffect, useState } from "react";
 
 type Props = {
   restArea: RestAreaWithInfo;
@@ -12,14 +14,30 @@ type Props = {
   onChange?: (restArea: RestAreaWithInfo) => void;
 };
 
-export function RestArea({ restArea, isEditing = false, onChange }: Props) {
-  const handleNameChange = (name: string) => console.log(name);
-  const handleUpdatedAtChange = (updatedAt: string) => console.log(updatedAt);
-  const handleStatusChange = (status: string) => console.log(status);
-  const handleDescriptionChange = (description: string) => console.log(description);
-  const handleLocalDescriptionChange = (localDescription: string) => console.log(localDescription);
-  const handleServiceRemove = (service: string) => console.log(service);
-  const handleServiceAdd = (service: string) => console.log(service);
+export function RestArea({ restArea: defaultRestArea, isEditing = false, onChange }: Props) {
+  const [restArea, setRestArea] = useState(defaultRestArea);
+  useEffect(() => {
+    setRestArea(defaultRestArea);
+  }, [defaultRestArea]);
+
+  const handleNameChange = (name: string) => setRestArea(prev => ({ ...prev, name }));
+  const handleUpdatedAtChange = (updatedAt: string) =>
+    setRestArea(prev => ({ ...prev, updated_at: updatedAt }));
+  const handleStatusChange = (status: Status) => setRestArea(prev => ({ ...prev, status }));
+  const handleDescriptionChange = (description: string) =>
+    setRestArea(prev => ({ ...prev, description }));
+  const handleLocalDescriptionChange = (localDescription: string) =>
+    setRestArea(prev => ({ ...prev, local_description: localDescription }));
+  const handleServiceRemove = (service: Service) =>
+    setRestArea(prev => ({
+      ...prev,
+      services: prev.services.filter(s => s.name !== service.name),
+    }));
+  const handleServiceAdd = (service: Service) =>
+    setRestArea(prev => ({
+      ...prev,
+      services: [...prev.services, service],
+    }));
 
   return (
     <div className="space-y-2 text-xs">
@@ -57,6 +75,7 @@ export function RestArea({ restArea, isEditing = false, onChange }: Props) {
       <RestAreaGallery photos={restArea.photos} />
 
       <RestAreaServices
+        restAreaId={restArea.id}
         services={restArea.services}
         isEditing={isEditing}
         onRemove={handleServiceRemove}
