b1926f28a78e4d0190b3b48a5a1044f31e1b0b5d
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-06-27 16:06:42 +0200

    feat(rest-areas): add ServiceDrawer for adding services in RestAreaServices

--- admin/src/features/rest-areas/components/rest-area/RestAreaServices.tsx
+++ admin/src/features/rest-areas/components/rest-area/RestAreaServices.tsx
@@ -1,21 +1,43 @@
 import { Button, ButtonIcon } from "@/components";
 import type { Service } from "../../types";
 import { Trash2 } from "lucide-react";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { useState, type PropsWithChildren } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 type Props = {
+  restAreaId: string;
   services: Service[];
   isEditing?: boolean;
   onRemove: (service: Service) => void;
   onAdd: (service: Service) => void;
 };
-export function RestAreaServices({ services, isEditing, onRemove, onAdd }: Props) {
+export function RestAreaServices({ restAreaId, services, isEditing, onRemove, onAdd }: Props) {
   return (
     <div>
       <div className="flex justify-between items-center">
         <span className="font-semibold">Tjänster:</span>
-        <Button size="sm" className="text-xs">
-          Lägg till tjänst
-        </Button>
+        {isEditing && (
+          <ServiceDrawer restAreaId={restAreaId} services={services} onAdd={onAdd}>
+            <Button variant="outline" className="text-xs">
+              Lägg till tjänst
+            </Button>
+          </ServiceDrawer>
+        )}
       </div>
       <div className="flex flex-row flex-wrap gap-2">
         {services.map(service => (
@@ -34,6 +56,77 @@ export function RestAreaServices({ services, isEditing, onRemove, onAdd }: Props
     </div>
   );
 }
+
+type ServiceDrawerProps = PropsWithChildren<{
+  restAreaId: string;
+  services: Service[];
+  onAdd: (service: Service) => void;
+}>;
+
+const ServiceDrawer = ({ restAreaId, services, onAdd, children }: ServiceDrawerProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [service, setService] = useState<Service>();
+  const handleChange = (value: Service) => setService(value);
+
+  const handleSave = () => {
+    if (service) {
+      onAdd(service);
+      setService(undefined);
+      setIsOpen(false);
+    }
+  };
+
+  return (
+    <Drawer open={isOpen} onOpenChange={setIsOpen} direction="right">
+      <DrawerTrigger asChild>{children}</DrawerTrigger>
+      <DrawerContent>
+        <div className="m-auto max-w-[400px]">
+          <DrawerHeader>
+            <DrawerTitle>Lägg till tjänst</DrawerTitle>
+          </DrawerHeader>
+          <ServiceSelector restAreaId={restAreaId} services={services} onChange={handleChange} />
+          <DrawerFooter>
+            <Button disabled={!service} onClick={handleSave}>
+              Spara
+            </Button>
+            <DrawerClose asChild>
+              <Button variant="outline">Stäng</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+type ServiceSelectorProps = {
+  restAreaId: string;
+  services: Service[];
+  onChange: (value: Service) => void;
+};
+const ServiceSelector = ({ restAreaId, services, onChange }: ServiceSelectorProps) => {
+  const handleChange = (value: string) => {
+    const service: Service = { name: value as Service["name"], rest_area_id: restAreaId };
+    onChange(service);
+  };
+
+  return (
+    <Select onValueChange={handleChange}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Välj tjänst" />
+      </SelectTrigger>
+      <SelectContent>
+        {Object.entries(SERVICE_NAME_MAP)
+          .filter(([key]) => !services.some(s => s.name === key))
+          .map(([key, value]) => (
+            <SelectItem key={key} value={key}>
+              <span className="font-semibol">{value}</span>
+            </SelectItem>
+          ))}
+      </SelectContent>
+    </Select>
+  );
+};
 
 const SERVICE_NAME_MAP: Record<Service["name"], string> = {
   refuseBin: "Sophantering",
