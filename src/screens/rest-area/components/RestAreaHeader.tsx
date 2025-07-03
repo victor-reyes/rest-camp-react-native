cfd66d88bc45546375e4c56a145c8eb2abed7d6b
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-07-03 09:18:10 +0200

    feat(parking-header): display score in heading

--- src/screens/rest-area/components/RestAreaHeader.tsx
+++ src/screens/rest-area/components/RestAreaHeader.tsx
@@ -1,30 +1,36 @@
 import { View, Text, StyleSheet, Pressable } from "react-native";
 import { openMap } from "@/navigation/open-map";
 import { MapIcon } from "@/components/icons";
+import { Score } from "./Score";
 
 interface Props {
+  id: string;
   name: string;
   updatedAt: number;
   latitude: number;
   longitude: number;
 }
 
-export function ParkingHeader({ name, updatedAt, latitude, longitude }: Props) {
+export function ParkingHeader({ id, name, updatedAt, latitude, longitude }: Props) {
   const handleOpenMap = () => openMap({ latitude, longitude, label: name });
+  const handleScoreClick = () => {};
   return (
     <View style={styles.headerContainer}>
       <View>
         <Text style={styles.headerTitle}>{name}</Text>
+        <Score restAreaId={id} onClick={handleScoreClick} />
+      </View>
+
+      <View>
+        <Pressable
+          style={({ pressed }) => [styles.iconButton, pressed && styles.iconButtonPressed]}
+          onPress={handleOpenMap}>
+          <MapIcon width={34} height={34} />
+        </Pressable>
         <Text style={styles.footerText}>
           Senast ändrad: {new Date(updatedAt).toLocaleDateString()}
         </Text>
       </View>
-
-      <Pressable
-        style={({ pressed }) => [styles.iconButton, pressed && styles.iconButtonPressed]}
-        onPress={handleOpenMap}>
-        <MapIcon width={34} height={34} />
-      </Pressable>
     </View>
   );
 }
@@ -46,6 +52,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   iconButton: {
+    alignSelf: "flex-end",
     elevation: 2,
     borderRadius: 50,
     padding: 6,
@@ -54,6 +61,7 @@ const styles = StyleSheet.create({
   iconButtonPressed: { backgroundColor: "#eeeeee" },
 
   footerText: {
+    marginTop: 4,
     fontSize: 6,
     color: "#666666",
   },
